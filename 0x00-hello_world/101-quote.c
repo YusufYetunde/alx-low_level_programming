76171329824448d0b30d0c91851b1c9e61212f5b
YusufYetunde <yusufyetunde13@gmail.com>
2023-02-16 14:18:47 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - entry level
- *
- * Return : 0 (success)
+ *  * main - A program that prints a line with puts function
+ *   * Return: 1 (approve)
  */
+
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
