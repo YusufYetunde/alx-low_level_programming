fe31351132f2a0a094adba0b0e99ee3dc40c3de0
YusufYetunde <yusufyetunde13@gmail.com>
2023-02-16 13:32:02 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return : Always 0 (Success)
+ *  * main - A program that prints a line with puts function
+ *   * Return: 0 (approve)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
