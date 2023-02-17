6eccc1303e1330f0b7d3be97b80c74697d8edb77
YusufYetunde <yusufyetunde13@gmail.com>
2023-02-17 11:57:03 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry level
+ *
+ * Description: 'Program to print single digit'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
