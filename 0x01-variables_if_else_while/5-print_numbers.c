75f4edcd82316a20354e55d868444e7ab4213589
YusufYetunde <yusufyetunde13@gmail.com>
2023-02-17 11:34:24 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry level
+ *
+ * Descriptiom: 'Program to print single digits'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
