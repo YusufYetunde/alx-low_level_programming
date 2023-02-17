4483d5fa1ce4f105c8f07421965abd1f782d831d
YusufYetunde <yusufyetunde13@gmail.com>
2023-02-17 10:50:02 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry level
+ *
+ * Description: 'program to print alphavet in lowwecase'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
