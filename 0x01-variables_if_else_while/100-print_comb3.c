64414716bd2a513725a32cf39e2cbf522796bf1a
YusufYetunde <yusufyetunde13@gmail.com>
2023-02-17 14:12:01 -0800

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry level
+ *
+ * Description: 'program tgat print combination'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c = 0;
+	int d = 0;
+
+	while (c < 10)
+	{
+		if (d != c && d < c)
+		{
+			putchar(d + '0');
+			putchar(c + '0');
+
+			if (c + d != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		c++;
+	}
+	d++;
+	putchar('\n');
+	return (0);
+}
