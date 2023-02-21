a79170bb0f5293f49fc88c2a8180e2760d1a9bd6
YusufYetunde <yusufyetunde13@gmail.com>
2023-02-21 10:12:02 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * main - Entry level
+ * Description: 'print_alphabet to print lowercase'
+ * Return: Always  0 (approve)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
