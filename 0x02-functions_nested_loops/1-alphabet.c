37deef4d1e24b3f26423de0bfce2340cf06b3d16
YusufYetunde <yusufyetunde13@gmail.com>
2023-02-21 11:28:09 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ * print_alphabet - Prints 10 times the alphabet, in lowercase.
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 	char c = 'a';
 
