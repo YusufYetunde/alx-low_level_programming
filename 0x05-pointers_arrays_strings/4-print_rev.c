1eb2659e564b001987354e786b4191fce95b19f2
YusufYetunde <yusufyetunde13@gmail.com>
2023-02-27 05:25:11 -0800

    Tadk 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,14 +3,20 @@
 /**
  * print_rev - Prints a string in reverse.
  * @s: The string to be printed.
+ * Return: string reverse
  */
 void print_rev(char *s)
 {
-	int lenght = 0, index;
+	int lenght = 0;
+	int i, n;
 
-	while (s[index++])
+	for (i = 0; s[i] != '\0'; i++)
+	{
 		lenght++;
-	for (index = lenght - 1; index >= 0; index--)
-		_putchar(s[index]);
+	}
+	for (n = (lenght - 1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
 	_putchar('\n');
 }
