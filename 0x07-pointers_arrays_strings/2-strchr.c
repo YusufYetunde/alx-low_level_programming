d5befc98d765deb1ec795cc3f259a2c3f62dd6de
YusufYetunde <yusufyetunde13@gmail.com>
2023-03-06 07:42:31 -0800

    task 3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * *_strchr -main entry.
+ * @s:char pointer
+ * @c: char variable
+ * Description: function that locates a character in a string
+ * Return:char pointer
+ */
+char *_strchr(char *s, char c)
+{
+	char *p = s;
+
+	for (; *p != '\0'; p++)
+	{
+		if (*p == c)
+		{
+			break;
+		}
+	}
+	if (*p == c)
+	{
+		return (p);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
