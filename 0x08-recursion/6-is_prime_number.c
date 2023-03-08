3bef9862ace4a5a403d379c6ad4e4e1d061c5597
YusufYetunde <yusufyetunde13@gmail.com>
2023-03-08 09:15:37 -0800

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,22 +13,15 @@ int find_prime(int x, int y)
 		return (0);
 	if (y * y > x)
 		return (1);
-
-
 	if (x % y == 0)
 		return (0);
-
 	else
 		return (find_prime(x, y + 1));
 }
-
 /**
  * is_prime_number -main entry.
- *
  * @n:char pointer
- *
  * Description:Function that checks if a number is a prime number
- * 
  * Return:1 if is prime otherwise return 0
  */
 int is_prime_number(int n)
