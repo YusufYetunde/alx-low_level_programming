a2f442bc260fc035418f61ffd3102dac3053d2fd
YusufYetunde <yusufyetunde13@gmail.com>
2023-03-10 11:43:52 -0800

    Task 3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -main entry.
+ * @argc:number of command line arguments
+ * @argv: array containing the program command line arguments
+ * Description:program that prints all arguments it receives.
+ * Return:integer
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[c]);
+	return (0);
+}
