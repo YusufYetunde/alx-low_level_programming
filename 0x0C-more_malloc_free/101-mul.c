fb49261278cb718c4135c30cbe4a2678af16b66f
YusufYetunde <yusufyetunde13@gmail.com>
2023-03-16 19:41:48 -0700

    task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -9,22 +8,26 @@
  */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+	unsigned long mul;
+	int num1, num2;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (num1 = 1; num1 < argc; num1++)
+	{
+		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+		{
+			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
 	}
 	mul = atol(argv[1]) *  atol(argv[2]);
 	printf("%lu\n", mul);
-return (0);
+	return (0);
 }
