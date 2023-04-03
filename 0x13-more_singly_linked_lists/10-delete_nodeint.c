462f094d8e4ddabbdc7114fd291c78c922ae6265
Yusuf Yetunde <yusufyetunde13@gmail.com>
2023-04-03 16:49:15 -0700

    funtions to delete node at index of a linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,26 +9,26 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *first, *second;
+	listint_t *current, *next;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
 	if (index == 0)
 	{
-		second = (*head)->second;
+		next = (*head)->next;
 		free(*head);
-		*head = second;
+		*head = next;
 		return (1);
 	}
-	first = *head;
+	current = *head;
 	for (i = 0; i < index - 1; i++)
 	{
-		if (first->second == NULL)
+		if (current->next == NULL)
 			return (-1);
-		first = first->second;
+		current = current->next;
 	}
-	second = first->second;
-	first->second = second->second;
-	free(second);
+	next = current->next;
+	current->next = next-> next;
+	free(next);
 	return (1);
 }
