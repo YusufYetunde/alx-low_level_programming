33e7135a53fb2a3254444996e20dbb36ef80638e
Yusuf Yetunde <yusufyetunde13@gmail.com>
2023-04-08 22:44:36 -0700

    function to print binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,14 +13,13 @@ unsigned int binary_to_uint(const char *b)
 	{
 		return (0);
 	}
-	while (*b != '\0')
+	for (i = 0; b[i]; i++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[i] < '0' || b[i] > '1')
 		{
 			return (0);
 		}
-		result = (result << 1) + (*b - '0');
-		++b;
+		result = 2 * result + (b[i] - '0');
 	}
 	return (result);
 }
