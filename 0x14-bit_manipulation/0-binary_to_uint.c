2468126df2a10288fe7884f20edc13fd8bfc1614
Yusuf Yetunde <yusufyetunde13@gmail.com>
2023-04-08 23:09:41 -0700

    Function to convert binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stddef.h>
 /**
  * binary_to_uint -  function that converts a binary number
  * @b: pointing to string of 0 and 1
@@ -7,17 +7,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
 	unsigned int result = 0;
 
-	if (!b)
-		return (0);
-
-	for (i = 0; b[i]; i++)
+	if (b == NULL)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		return (0);
+	}
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+		{
 			return (0);
-		result = 2 * result + (b[i] - '0');
+		}
+		result = (result << 1) + (*b - '0');
+		++b;
 	}
 	return (result);
 }
